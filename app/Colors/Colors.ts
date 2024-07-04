a998730ca115a9eee0873b5419c12f259507a1d4
Dachikk <dachikk.official@gmail.com>
2024-07-04 16:44:01 +0400

    Mukhambazi-FRONT

---
+++ app/Colors/Colors.ts
@@ -0,0 +1,11 @@
+const colors = {
+    yellow: "#f1c40f",
+    red: "#e74c3c",
+    white: "#ffffff",
+    blue: "#3498db",
+    green: "#2ecc71",
+    purple: "#9b59b6"
+  };
+  
+  export default colors;
+  
---
+++ app/Colors/ColorsEnums.ts
@@ -0,0 +1,9 @@
+export enum Colors {
+    Yellow = "yellow",
+    Red = "red",
+    White = "white",
+    Blue = "blue",
+    Green = "green",
+    Purple = "purple"
+  }
+  
