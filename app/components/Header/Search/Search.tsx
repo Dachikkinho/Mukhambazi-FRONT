461a9162518b26587d97636575dd7556bf6718c1
Dachikkinho <157109822+Dachikkinho@users.noreply.github.com>
2024-07-05 19:04:33 +0400

    NAVI-31/feat/Added-Reusable-Input

---
+++ app/components/Header/Search/Search.tsx
@@ -0,0 +1,19 @@
+import styles from "./Search.module.scss"
+
+type Props = {
+   placeholder: string;
+   icon: string;
+   width: number;
+   height: number;
+}
+
+export const Search = ({placeholder, icon, width, height}: Props) => {
+    return (
+        <div className={styles.container}>
+            <input type="text" placeholder={placeholder} className={styles.mainSearch} />
+            <img src={`icons/${icon}.svg`} alt="icon" className={styles.searchIcon} width={width} height={height}/>
+        </div>
+    )
+}
+
+
