982bb37dc3f4030f083fb2bc16879b97ae497984
Dachikkinho <157109822+Dachikkinho@users.noreply.github.com>
2024-07-05 14:30:21 +0400

    NAVI-27/feat/ArtistCard-Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 
 import Album, { AlbumCard } from "./AlbumCard/AlbumCard";
+import { ArtistCard } from "./ArtistCard/Artistcard";
+
 import { TopHits } from "./TopHits/TopHits";
 import styles from "./page.module.css";
 
