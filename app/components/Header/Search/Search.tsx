83e2f958d93a1551e72f1061d464be92fdbc5b69
Dachikk <dachikk.official@gmail.com>
2024-07-08 13:49:47 +0400

    feat:/Added-onchange-on/Search.tsx

--- app/components/Header/Search/Search.tsx
+++ app/components/Header/Search/Search.tsx
@@ -1,19 +1,29 @@
 import styles from "./Search.module.scss"
 
 type Props = {
-   placeholder: string;
-   icon: string;
-   width: number;
-   height: number;
+    placeholder: string;
+    icon: string;
+    width: number;
+    height: number;
+    onChange: (e: any) => void;
 }
 
-export const Search = ({placeholder, icon, width, height}: Props) => {
+export const Search = ({ placeholder, icon, width, height, onChange }: Props) => {
     return (
         <div className={styles.container}>
-            <input type="text" placeholder={placeholder} className={styles.mainSearch} />
-            <img src={`icons/${icon}.svg`} alt="icon" className={styles.searchIcon} width={width} height={height}/>
+            <input
+                type="text"
+                placeholder={placeholder}
+                className={styles.mainSearch}
+                onChange={onChange}
+            />
+            <img
+                src={`icons/${icon}.svg`}
+                alt="icon"
+                className={styles.searchIcon}
+                width={width}
+                height={height}
+            />
         </div>
     )
 }
-
-
