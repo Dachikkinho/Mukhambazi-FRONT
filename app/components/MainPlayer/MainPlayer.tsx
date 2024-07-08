528b9aa3d09293b001a2042f5d3640cb6d721bfd
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2024-07-08 16:47:28 +0400

    Navi-26/Feat/Player

---
+++ app/components/MainPlayer/MainPlayer.tsx
@@ -0,0 +1,69 @@
+'use client'
+import H5AudioPlayer, { RHAP_UI } from "react-h5-audio-player"
+import { PreviousNext } from "./PreviousNext/PreviousNext";
+import styles from "./MainPlayer.module.scss"
+import { useState, useEffect } from "react";
+import { Shuffle } from "./Shuffle/Shuffle";
+
+
+
+export default function MainPlayer () {
+    const [isShuffle, setIsShuffle] = useState(false);
+    
+    const switchIsShuffle = () => {
+        setIsShuffle(!isShuffle)
+    }
+
+    // Phone Controls
+
+    const [isMobile, setIsMobile] = useState(false)
+
+    useEffect(() => {
+        setIsMobile(window.innerWidth <= 768)
+        const handleResize = () => {
+        setIsMobile(window.innerWidth <= 768);
+      };
+  
+      window.addEventListener('resize', handleResize);
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+    
+    const mobileControlsBottom = [
+        RHAP_UI.MAIN_CONTROLS,
+        RHAP_UI.VOLUME
+    ]
+
+    const mobileVolume = !isMobile && RHAP_UI.VOLUME;
+
+    const mobileControls = !isMobile && RHAP_UI.MAIN_CONTROLS;
+
+    return (
+        <H5AudioPlayer 
+        className={`${styles.player} ${styles.active}`} 
+        customControlsSection={isMobile ? mobileControlsBottom : []}
+        customProgressBarSection={[
+            mobileControls,
+            RHAP_UI.CURRENT_TIME, 
+            RHAP_UI.PROGRESS_BAR, 
+            RHAP_UI.DURATION, 
+            RHAP_UI.ADDITIONAL_CONTROLS,
+            RHAP_UI.LOOP,
+            mobileVolume,
+            ]}
+        customAdditionalControls={[<Shuffle onClick={switchIsShuffle} isActive={isShuffle}/>]}
+        showSkipControls
+        showJumpControls={false}
+        src={""}
+        customIcons={{
+            next: <PreviousNext />,
+            previous: <PreviousNext isPrev/>,
+            pause: <img src="/icons/playerIcons/playing.svg" className={styles.pause} alt="icon"/>,
+            volume: <img src="/icons/playerIcons/aduioOn.svg" className={styles.play} alt="icon"/>,
+            volumeMute: <img src="/icons/playerIcons/audioOff.svg" className={styles.play} alt="icon"/>,
+            loopOff: <img src="/icons/playerIcons/repeat-single 1.svg" className={styles.loop} alt="icon"/>,
+        }}
+        />
+    )
+}
---
+++ app/components/MainPlayer/PreviousNext/PreviousNext.tsx
@@ -0,0 +1,13 @@
+import styles from "./PreviousNext.module.scss"
+
+type Props = {
+    isPrev?: boolean
+}
+
+export function PreviousNext ({isPrev}: Props) {
+    return (
+        <div className={styles.container}>
+            {isPrev ? <img src="/icons/playerIcons/previous.svg" alt="" /> : <img src="/icons/playerIcons/next.svg" alt="icon" />}
+        </div>
+    )
+}
