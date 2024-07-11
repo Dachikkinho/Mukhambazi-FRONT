d125a5b0aec61bcc6092926c3dfe7d6e3c83198b
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2024-07-11 18:18:10 +0400

    Navi/Refactor/Side-bar

--- app/components/SideBar/SidebarNav/SidebarNavItems/SidebarNavItems.tsx
+++ app/components/SideBar/SidebarNav/SidebarNavItems/SidebarNavItems.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 type Props = {
     link: string,
     img: string,
-    name: string
+    name: string,
 }
 
 export function SidebarNavItems({link, img, name}: Props) {
     return (
         <li>
-            <Link href={link} className={styles.link}>
+            <Link href={link} className={styles.link} >
                 <img src={`/icons/${img}.svg`} alt="icon" />
                 <p>{name}</p>
             </Link>
---
+++ app/components/SideBar/SidebarSelected/SidebarSelected.tsx
@@ -0,0 +1,50 @@
+'use client'
+import styles from "./SidebarSelected.module.scss"
+import { usePathname } from "next/navigation"
+import { useEffect, useState } from "react"
+
+export function SidebarSelected() {
+
+    const path = usePathname();
+    const [position, setPosition] = useState("151px");
+
+    useEffect(() => {
+        console.log(path);
+
+        switch (path) {
+            case "/":
+                setPosition("151px");
+                break;
+            case "/artists":
+                setPosition("195px");
+                break;
+            case '/albums':
+                setPosition("240px");
+                break;
+            case "/songs":
+                setPosition("284px");
+                break;
+            case "/playlist":
+                setPosition("407px");
+                break;
+            case "/specials":
+                setPosition("451px");
+                break;
+            case "/favorites":
+                setPosition("496px");
+                break;
+            case "/podcasts":
+                setPosition("540px");
+                break;
+            default:
+                setPosition("151px");
+        }
+    }, [path])
+
+    return (
+        <div className={styles.main} style={{
+            top: position
+        }}>
+        </div>
+    )
+}
