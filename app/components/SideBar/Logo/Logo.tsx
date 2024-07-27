a120cb851bfc12c64cf8e851dffd5be74cf6385f
Dachikk <dachikk.official@gmail.com>
2024-07-27 13:50:51 +0400

    NAVI/Presentation

--- app/components/SideBar/Logo/Logo.tsx
+++ app/components/SideBar/Logo/Logo.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import styles from './Logo.module.scss';
 
 const Logo = () => {
     return (
         <div className={styles.container}>
             <h1 className={styles.logo}>CHAKRULOS</h1>
-            <p className={styles.subName}>By NaVi™</p>
+            <Link href="/natusvincere">
+                <p className={styles.subName}>By NaVi™</p>
+            </Link>
         </div>
     );
 };
