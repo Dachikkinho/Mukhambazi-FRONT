2ef958bb688c80ed530f499b048a311ac2e2eb25
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2024-09-13 16:49:29 +0400

    Navi/Login-Error-Fix

--- app/(notauthorised)/login/page.tsx
+++ app/(notauthorised)/login/page.tsx
@@ -81,10 +81,28 @@ const Login = () => {
                         message: 'Invalid email or password. Please try again.',
                     });
                 } else if (axiosError.response.status === 401) {
-                    setError('password', {
-                        type: 'manual',
-                        message: 'Your email has been banned from CHAKRULOS!',
-                    });
+                    console.log(axiosError.response);
+                    if (
+                        (axiosError.response?.data as { message?: string })
+                            ?.message === 'USER IS BLOCKED'
+                    ) {
+                        setError('password', {
+                            type: 'manual',
+                            message:
+                                'Your email has been banned from CHAKRULOS!',
+                        });
+                    } else {
+                        setError('email', {
+                            type: 'manual',
+                            message:
+                                'Invalid email or password. Please try again.',
+                        });
+                        setError('password', {
+                            type: 'manual',
+                            message:
+                                'Invalid email or password. Please try again.',
+                        });
+                    }
                 } else {
                     console.error(
                         'An error occurred during login:',
